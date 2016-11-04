29215a3289547cdfcaf5f5ad4bc5de506be46633
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2016-11-04 10:35:10 +0100

    [REVIEW] Simplification of JS
    (cherry picked from commit d543edc)

--- web-samples/index.js
+++ web-samples/index.js
@@ -1,40 +1,59 @@
 (function (document) {
     'use strict';
 
+    function $(selector){
+        return document.querySelector(selector);
+    }
+
     window.addEventListener('WebComponentsReady', function () {
 
-        var writeHere = document.querySelector('.write-here');
+        var textInput = $('#textInput');
+        var mathInput = $('#mathInput');
+        var graphicsInput = $('#graphicsInput');
+        var musicInput = $('#musicInput');
+        var musicXML = $('#MUSICXML');
+        var textButton = $('#textButton');
+        var mathButton = $('#mathButton');
+        var graphicsButton = $('#graphicsButton');
+        var musicButton = $('#musicButton');
 
-        var textInput = document.getElementById('textInput'),
-            mathInput = document.getElementById('mathInput'),
-            graphicsInput = document.getElementById('graphicsInput'),
-            musicInput = document.getElementById('musicInput'),
-            musicXML = document.getElementById('MUSICXML');
-
-        var textButton = document.getElementById('textButton'),
-            mathButton = document.getElementById('mathButton'),
-            graphicsButton = document.getElementById('graphicsButton'),
-            musicButton = document.getElementById('musicButton');
-
+        // Small piece of code to hide the write here message
         var hideWriteHere = function(input){
             input.addEventListener('pointerdown', function () {
-                if (writeHere) {
-                    writeHere.setAttribute('hidden', 'true');
-                }
+                    $('.write-here').setAttribute('hidden', 'true');
             });
         };
+        var listOfInputs = [textInput, mathInput, graphicsInput, musicInput];
+        listOfInputs.forEach(function(input){hideWriteHere(input)});
 
+        // Manage the tap on the various buttons
         var setButtonsStates = function (isTextButton, isMathButton, isGraphicsButton, isMusicButton){
+            listOfInputs.forEach(function(input){input.setAttribute('hidden', 'true');});
+            musicXML.setAttribute('hidden', 'true');
+
             textButton.active = isTextButton;
+            if(isTextButton)textInput.removeAttribute('hidden');
+
             mathButton.active = isMathButton;
+            if(isMathButton) mathInput.removeAttribute('hidden');
+
             graphicsButton.active = isGraphicsButton;
+            if(isGraphicsButton) graphicsInput.removeAttribute('hidden');
+
             musicButton.active = isMusicButton;
+            if(isMusicButton){
+                musicInput.removeAttribute('hidden');
+                musicXML.removeAttribute('hidden');
+            }
+            $('.write-here').removeAttribute('hidden');
         };
 
         var initMusic = function (musicInput, musicXML){
+            // Setting the mandatory parameters for music input.
             musicInput.components = [new MyScript.MusicClefInputComponent({value: {yAnchor: 160,octave: 0,symbol: 'G'},boundingBox: {x: 5,y: 72,height: 140}})];
             musicInput.musicparameters = new MyScript.MusicParameter({divisions: 480,staff: {count: 5,gap: 20,top: 100},resultTypes: [MyScript.ResultType.Music.MUSICXML, MyScript.ResultType.Music.SCORETREE]});
 
+            //Register the common element result event to print the MusicXML output in a textfield.
             musicInput.addEventListener('myscript-common-element-result', function (e) {
                 musicXML.innerText = '';
                 if (e.detail && e.detail.getDocument) {
@@ -52,51 +71,21 @@
         // Initialize the default demo
         textButton.active = true;
 
-        // Small piece of code to hide the write here message
-        hideWriteHere(textInput);
-        hideWriteHere(mathInput);
-        hideWriteHere(graphicsInput);
-        hideWriteHere(musicInput);
-
         textButton.addEventListener('tap', function () {
             setButtonsStates(true, false, false, false);
-            textInput.removeAttribute('hidden');
-            mathInput.setAttribute('hidden', 'true');
-            graphicsInput.setAttribute('hidden', 'true');
-            musicInput.setAttribute('hidden', 'true');
-            musicXML.setAttribute('hidden', 'true');
             textInput.delete();
-            writeHere.removeAttribute('hidden');
         });
         mathButton.addEventListener('tap', function () {
             setButtonsStates(false, true, false, false);
-            textInput.setAttribute('hidden', 'true');
-            mathInput.removeAttribute('hidden');
-            graphicsInput.setAttribute('hidden', 'true');
-            musicInput.setAttribute('hidden', 'true');
-            musicXML.setAttribute('hidden', 'true');
             mathInput.delete();
-            writeHere.removeAttribute('hidden');
         });
         graphicsButton.addEventListener('tap', function () {
             setButtonsStates(false, false, true, false);
-            textInput.setAttribute('hidden', 'true');
-            mathInput.setAttribute('hidden', 'true');
-            graphicsInput.removeAttribute('hidden');
-            musicInput.setAttribute('hidden', 'true');
-            musicXML.setAttribute('hidden', 'true');
             graphicsInput.clear();
-            writeHere.removeAttribute('hidden');
         });
         musicButton.addEventListener('tap', function () {
             setButtonsStates(false, false, false, true);
-            textInput.setAttribute('hidden', 'true');
-            mathInput.setAttribute('hidden', 'true');
-            graphicsInput.setAttribute('hidden', 'true');
-            musicInput.removeAttribute('hidden');
-            musicXML.removeAttribute('hidden');
             musicInput.clear();
-            writeHere.removeAttribute('hidden');
             initMusic(musicInput, musicXML);
         });
     });
