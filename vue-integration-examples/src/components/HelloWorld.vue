219f21b397a70c2e464befa458dd75987475708c
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2018-02-01 15:12:37 +0100

    Add a first basci example with VueJS

---
+++ vue-integration-examples/src/components/HelloWorld.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="/#/myscript-js"
+        >
+          MyScriptJS in action
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ vue-integration-examples/src/components/myscriptjs-vue-component.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div class="writing-container" ref="editor"></div>
+  </div>
+</template>
+
+<script>
+import * as MyScriptJS from 'myscript/src/myscript';
+
+export default {
+  name: 'MyScriptJSVueComponent',
+  data() {
+    return {
+      msg: 'Basic example of vue integration of MyScriptJS',
+    };
+  },
+  created() {
+    // eslint-disable-next-line
+    console.log("Created" + this.$refs['editor']);
+  },
+  mounted() {
+    // Fired every second, should always be true
+    // eslint-disable-next-line
+    console.log("Mounted " + this.$refs['editor']);
+    MyScriptJS.register(this.$refs.editor, {
+      recognitionParams: {
+        type: 'TEXT',
+        protocol: 'WEBSOCKET',
+        apiVersion: 'V4',
+        server: {
+          scheme: 'https',
+          host: 'webdemoapi.myscript.com',
+          applicationKey: 'xxxx-xxxx-xxxx-xxxx',
+          hmacKey: 'xxxx-xxxx-xxxx-xxxx',
+        },
+      },
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+@import url('myscript/dist/myscript.min.css');
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.writing-container {
+  min-height: 600px;
+  min-width: 300px;
+}
+</style>
