c979cb4372fe7d764737ab03b8ae603bedd1146e
DEWITTE Pierre-Alban <pierre-alban.dewitte@myscript.com>
2018-10-21 22:39:15 +0200

    [batch mode] Commiting a first build to collect internal feedback

--- batch-mode-explorer/README.md
+++ batch-mode-explorer/README.md
@@ -1,16 +1,1 @@
-# MyScript Web Integration Samples
 
-In this repository, you will find **integration samples** of myscript web APIs.
-
-## Integration samples list
-- __[angular-integration-examples](./angular-integration-examples) :__ A basic integration of MyScriptJS into an Angular application.
-- __[react-integration-examples](./react-integration-examples) :__ A basic integration of MyScriptJS into a React application.
-- __[vue-integration-examples](./vue-integration-examples) :__ A basic integration of MyScriptJS into a Vue application.
-- __[word-add-in-sample](./word-add-in-sample) :__ A complete integration sample of [myscript-math-web](https://github.com/MyScript/myscript-math-web) in a Microsoft Word add-in. You can try it directly in Microsoft Word 2013 or newer version. Complete description available in [Microsoft Office Store](https://store.office.com/en-us/app.aspx?assetid=WA104380646).
-
-## Try the samples
-Once you have clone this repository, each sub-directories contains a README.md files with instructions to run the sample.
-
-## License
-
-[Apache License 2.0](http://www.apache.org/licenses/LICENSE-2.0)
---
+++ batch-mode-explorer/dist/precache-manifest.4d45f65a697b73d467283d7ff7504ace.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ecde38e4c22d11e9e67a48dc6fed94",
+    "url": "img/redo.a6ecde38.svg"
+  },
+  {
+    "revision": "ea5c5769d05107f8537124623f5c6737",
+    "url": "index.html"
+  },
+  {
+    "revision": "4b5e845bbc3c3cc98762",
+    "url": "js/app.140936bc.js"
+  },
+  {
+    "revision": "64e44afbcd8c7583ee1a",
+    "url": "js/chunk-vendors.f194682c.js"
+  },
+  {
+    "revision": "b02f352957b694308f57d45454cf8374",
+    "url": "img/clear.b02f3529.svg"
+  },
+  {
+    "revision": "164ec88d41c870607d54431689abee83",
+    "url": "img/menu.164ec88d.svg"
+  },
+  {
+    "revision": "998cec381cb661b2644da0a77611b58c",
+    "url": "img/myscript-logo.998cec38.svg"
+  },
+  {
+    "revision": "59ab40053bf4fe002c52d47ef324d41a",
+    "url": "img/undo.59ab4005.svg"
+  },
+  {
+    "revision": "085f3e08808b54ddc018366032145390",
+    "url": "img/pen.085f3e08.svg"
+  },
+  {
+    "revision": "0189454f9c05ddf3923cc969bef65131",
+    "url": "img/eraser.0189454f.svg"
+  },
+  {
+    "revision": "091f7f662ec40750cb99b071603f8c61",
+    "url": "img/add.091f7f66.svg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "64e44afbcd8c7583ee1a",
+    "url": "css/chunk-vendors.6735b12a.css"
+  },
+  {
+    "revision": "4b5e845bbc3c3cc98762",
+    "url": "css/app.9d69b357.css"
+  }
+];
